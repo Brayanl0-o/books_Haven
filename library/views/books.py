3af12c35c73f3cb8c9d5e75fc6570203fff485a7
Brayan <brayantandap@gmail.com>
2023-12-30 14:28:53 -0500

    updated > books.py (views)

--- library/views/books.py
+++ library/views/books.py
@@ -58,15 +58,23 @@ class BookEditView(UpdateView):
     model = Book
     template_name = 'library/book/book_edit.html'
     fields = ['name', 'author','cover_page' , 'link_dowload_free','link_dowload_buy', 'genre', 'release_date', 'number_pages', 'summary']
-    def form_valid(self, form):
-        # Sobrescribe el método form_valid para personalizar el comportamiento
-        response = super().form_valid(form)
 
-        # Asocia el libro con el autor
+    def form_valid(self, form):
+        # Obtiene el autor original antes de la edición
+        old_author = Book.objects.get(pk=self.object.pk).author
+
+        # Desvincula el libro del autor anterior antes de asociarlo al nuevo autor
+        if old_author:
+            old_author.books.remove(self.object)
+            print(f"Libro desvinculado del autor anterior: {self.object} del autor: {old_author.name}")
+
+        # Asocia el libro con el nuevo autor
+        response = super().form_valid(form)
         author_id = self.request.POST.get('author')
         if author_id:
-            author = get_object_or_404(Author, pk=author_id)
-            author.books.add(self.object)
+            new_author = get_object_or_404(Author, pk=author_id)
+            new_author.books.add(self.object)
+            print(f"Libro asociado al nuevo autor: {self.object} al autor: {new_author.name}")
 
         return response
     def get_success_url(self):
