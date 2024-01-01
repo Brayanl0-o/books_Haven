5deb914aa194bb4db4cafba8efeede647d7409e6
Brayan <brayantandap@gmail.com>
2024-01-01 13:57:55 -0500

    updated > wsgin.py

--- booksHaven/wsgi.py
+++ booksHaven/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for libraryCuyeca project.
+WSGI config for booksHaven project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'libraryCuyeca.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booksHaven.settings')
 
 application = get_wsgi_application()
