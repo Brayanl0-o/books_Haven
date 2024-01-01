ba6bff935ac4ba6b39827255ea0146f12e8dd58c
Brayan <brayantandap@gmail.com>
2024-01-01 14:25:36 -0500

    updated > wsgi.py

--- booksHaven/wsgi.py
+++ booksHaven/wsgi.py
@@ -8,6 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/Brayan101/booksHaven'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 
