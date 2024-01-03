aeb143cfae75f04b3e995122ffc42036a96c4b3e
Brayan <brayantandap@gmail.com>
2024-01-02 20:58:22 -0500

    updated

--- booksHaven/settings.py
+++ booksHaven/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'library/static'),
+    os.path.join(BASE_DIR, 'library/static/library'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
